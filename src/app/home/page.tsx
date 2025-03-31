3990568758478115c6ba3685adfc2bd95782920b
Mayank Piparde <mayankpiparde20@gmail.com>
2025-03-31 12:39:30 +0530

    setup

---
+++ src/app/home/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "@/components/header";
+import HomeHero from "@/components/homehero";
+import PoojaItems from "@/components/poojaitems";
+import HinduPoojas from "@/components/hindupoojas";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col min-h-screen w-full">
+      {/* Header (Sticky for better scrolling UX) */}
+      <header className="w-full sticky top-0 z-50">
+        <Header />
+      </header>
+
+      {/* Main Content (Centered and Responsive) */}
+      <main className="flex flex-col grow items-center w-full">
+        <HomeHero />
+        <PoojaItems />
+        <HinduPoojas />
+      </main>
+    </div>
+  );
+}
